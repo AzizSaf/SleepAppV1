b9fd95475787e26c579e77ec57d79c0e5207ac61
Aziz Saf <azizsaf40@gmail.com>
2024-03-24 14:44:09 +0000

    update all files

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ function openAlbum(e) {
       MusicList.append(MusicListBar)
       let Back =
         document.createElement('img')
-      Back.src = "/Src/MusicList/back.png"
+      Back.src = "./Src/MusicList/back.png"
       Back.setAttribute('onclick', 'closeMusicList()')
       MusicListBar.append(Back)
       let Title =
@@ -221,7 +221,7 @@ function openAlbum(e) {
         Music.append(MusicName)
         let Play =
           document.createElement('img')
-        Play.src = '/Src/MusicList/play.png'
+        Play.src = './Src/MusicList/play.png'
         Music.append(Play)
         let Musichr =
           document.createElement('hr')
@@ -257,7 +257,7 @@ function openMusicPalyer(e) {
   MusicPlayer.append(MusicPlayeBar)
   let Back =
     document.createElement('img')
-  Back.src = "/Src/MusicList/back.png"
+  Back.src = "./Src/MusicList/back.png"
   Back.setAttribute('onclick', 'closeMusicPlayer()')
   MusicPlayeBar.append(Back)
   let Title =
@@ -294,13 +294,13 @@ function openMusicPalyer(e) {
     document.createElement('div')
   AudioContol.className = 'AudioContol'
   AudioContol.innerHTML =
-    `<img src="/Src/MusicPlayer/left.png" class="BackBtn">
+    `<img src="./Src/MusicPlayer/left.png" class="BackBtn">
       <label class="container">
         <input type="checkbox">
         <svg viewBox="0 0 384 512" height="1em" xmlns="http://www.w3.org/2000/svg" class="play"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"></path></svg>
         <svg viewBox="0 0 320 512" height="1em" xmlns="http://www.w3.org/2000/svg" class="pause"><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"></path></svg>
       </label>
-    <img src="/Src/MusicPlayer/right.png" class="MoveBtn">`
+    <img src="./Src/MusicPlayer/right.png" class="MoveBtn">`
   MPlayerItems.append(AudioContol)
   let MusicAudio =
     document.createElement('div')
