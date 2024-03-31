e3dc76449a92cf35241343ee9d8143344b38e4bb
aziz saf <azizsaf40@gmail.com>
2024-03-31 14:31:30 +0000

    update All files

--- script.js
+++ script.js
@@ -7,12 +7,200 @@ let CloseMenu =
 let MenuBox =
   document.querySelector('.MenuBox')
 
-MenuBtn.addEventListener('click', () => {
+MenuBtn.addEventListener('click', openMenu)
+function openMenu() {
   MenuBox.style.left = '0%'
-})
-CloseMenu.addEventListener('click', () => {
+}
+CloseMenu.addEventListener('click', closeMenu)
+function closeMenu() {
   MenuBox.style.left = '-100%'
-})
+}
+
+// Menu Items
+let Profile =
+document.querySelector('.Profile')
+let ProfileBox =
+  document.querySelector('.ProfileBox')
+  
+Profile.addEventListener('click', openProfileBox)
+function openProfileBox() {
+  ProfileBox.style.display = 'flex'
+  setTimeout(() => {
+    ProfileBox.style.right = "0"
+  }, 40)
+  let ProfileNav =
+     document.createElement('div')
+  ProfileNav.className = 'ProfileNav'
+  ProfileBox.append(ProfileNav)
+  let Back =
+    document.createElement('img')
+  Back.src = "./Src/MusicList/back.png"
+  Back.setAttribute('onclick', 'closeProfileBox()')
+  ProfileNav.append(Back)
+  let Title =
+    document.createElement('h1')
+  Title.textContent = "Profile"
+  ProfileNav.append(Title)
+  let UserImg = 
+    document.createElement('div')
+  UserImg.innerHTML = 
+  `<img src="/Src/ProfileBox/user.png">`
+  UserImg.className = 'UserImg'
+  ProfileBox.append(UserImg)
+  let getUserData = JSON.parse(localStorage.getItem('UserInfo'))
+  let UserName =
+    document.createElement('div')
+  UserName.innerHTML =
+    `<p>${getUserData.userName}</p>`
+  UserName.className = 'UserName'
+  ProfileBox.append(UserName)
+  let UserInfo =
+    document.createElement('div')
+  UserInfo.innerHTML =
+    `<label>Your Email:</label>
+    <input type="text" readonly value=${getUserData.email}>
+    <br>
+    <label>Your Password:</label>
+    <input class="userPassword" type="password" readonly value=${getUserData.password}>`
+  UserInfo.className = 'UserInfo'
+  ProfileBox.append(UserInfo)
+  let Show =
+    document.createElement('img')
+  Show.src = "/Src/ProfileBox/show.png"
+  Show.className = 'ShowPasword'
+  Show.setAttribute('onclick', 'showPassword()')
+  UserInfo.append(Show)
+}
+function showPassword() {
+  let ShowImg =
+    document.querySelector('.ShowPasword')
+  let userPassword =
+  document.querySelector('.userPassword')
+  if (userPassword.type === 'text') {
+    userPassword.type = 'password'
+    ShowImg.src = "/Src/ProfileBox/show.png"
+  } else {
+    userPassword.type = 'text'
+    ShowImg.src = "/Src/ProfileBox/hied.png"
+  }
+}
+function closeProfileBox() {
+  ProfileBox.style.right = "-100%"
+  setTimeout(() => {
+    ProfileBox.style.display = 'none'
+    ProfileBox.innerHTML = ''
+  }, 200)
+}
+
+let Update =
+document.querySelector('.Update')
+let UpdateBox =
+  document.querySelector('.UpdateBox')
+  
+Update.addEventListener('click', openUpdateBox)
+let Updates = 
+[
+  {
+    version: '2.0',
+    info: "* Change App UI (Text, Colors, Animations...) <br>* Fix some issues. <br>* Optimize Home Page UI.<br>* Optimize Login Page UI."
+  },
+  {
+    version: '1.0',
+    info: "* Add First Page.<br>* Add Login Page.<br>* Add the Main Content...<br>* This is first version of my app, I promise i well add some new things in future."
+  }
+]
+function openUpdateBox() {
+  UpdateBox.style.display = 'flex'
+  setTimeout(() => {
+    UpdateBox.style.right = "0"
+  }, 40)
+  let ProfileNav =
+     document.createElement('div')
+  ProfileNav.className = 'UpdateNav'
+  UpdateBox.append(ProfileNav)
+  let Back =
+    document.createElement('img')
+  Back.src = "./Src/MusicList/back.png"
+  Back.setAttribute('onclick', 'closeUpdateBox()')
+  ProfileNav.append(Back)
+  let Title =
+    document.createElement('h1')
+  Title.textContent = "Updates"
+  ProfileNav.append(Title)
+  let UpdateDiv =
+    document.createElement('div')
+  UpdateDiv.className = 'UpdateDiv'
+  UpdateBox.append(UpdateDiv)
+  Updates.map(e => {
+    let Card =
+    document.createElement('div')
+    Card.className = 'Card'
+    Card.innerHTML =
+    `<p>Update Version: ${e.version}</p>
+    <p>Info:<br><span>${e.info}</span></p>`
+    UpdateDiv.append(Card)
+  
+  })
+}
+function closeUpdateBox() {
+  UpdateBox.style.right = "-100%"
+  setTimeout(() => {
+    UpdateBox.style.display = 'none'
+    UpdateBox.innerHTML = ''
+  }, 200)
+}
+
+let About =
+  document.querySelector('.About')
+let AboutBox =
+  document.querySelector('.AboutBox')
+
+About.addEventListener('click', openAboutBox)
+function openAboutBox() {
+  AboutBox.style.display = 'flex'
+  setTimeout(() => {
+    AboutBox.style.right = "0"
+  }, 40)
+  let ProfileNav =
+     document.createElement('div')
+  ProfileNav.className = 'AboutNav'
+  AboutBox.append(ProfileNav)
+  let Back =
+    document.createElement('img')
+  Back.src = "./Src/MusicList/back.png"
+  Back.setAttribute('onclick', 'closeAboutBox()')
+  ProfileNav.append(Back)
+  let Title =
+    document.createElement('h1')
+  Title.textContent = "About"
+  ProfileNav.append(Title)
+  let AboutDiv =
+    document.createElement('div')
+  AboutDiv.className = 'AboutDiv'
+  AboutBox.append(AboutDiv)
+  let AppInfo = 
+    document.createElement('div')
+  AppInfo.className = 'AppInfo'
+  AppInfo.innerHTML = "<p><strong>Sleep</strong> Is relaxation app is a digital tool designed to help users reduce stress, improve mental well-being, and promote relaxation through various features such as guided meditation, breathing exercises, soothing music, nature sounds, mindfulness activities, and sleep aids.</p><p>These apps often offer customizable experiences tailored to individual preferences and goals, providing a convenient way to incorporate relaxation techniques into daily routines.</p><p>Popular relaxation apps include Calm, Headspace, Insight Timer, and Relax Melodies, among others.</p>"
+  AboutDiv.append(AppInfo)
+  let Media =
+    document.createElement('div')
+  Media.className = 'Media'
+  Media.innerHTML = 
+  `<p>Follow Us</p>
+  <a target="_blanc" href="#"><img src="/Src/AboutBox/facebook.png"></a>
+  <a target="_blanc" href="#"><img src="/Src/AboutBox/instagram.png"></a>
+  <a target="_blanc" href="#"><img src="/Src/AboutBox/twitter.png"></a>`
+  AboutDiv.append(Media)
+}
+function closeAboutBox() {
+  AboutBox.style.right = "-100%"
+  setTimeout(() => {
+    AboutBox.style.display = 'none'
+    AboutBox.innerHTML = ''
+  }, 200)
+}
+
 
 // Wellcome Page 
 let WellcomePage =
@@ -31,6 +219,7 @@ if (isFirstTime === 'true') {
 } else {
   WellcomePage.style.display = 'flex'
 }
+  
   
 // Login Page 
 let LoginPage =
@@ -84,11 +273,12 @@ LoginBtn.addEventListener('click', () => {
     email: email,
     password: pass
   }
-  if (name !== ''
-      && email !== ''
-      && pass !== '' ) {
+  if (
+    email !== '' &&
+    pass !== '') {
     let UserData = localStorage.setItem('UserInfo', JSON.stringify(user)) | []
     LoginPage.style.display = 'none'
+    location.reload()
   }
 })
 
@@ -99,46 +289,62 @@ if (getUserData == undefined) {
 } else {
   LoginPage.style.display = 'none'
 }
- 
-// Search 
-/*let SearchText =
-  document.querySelector('.searchText')
-let SearchBtn =
-  document.querySelector('.SearchBtn')
-let SearchResults =
-  document.querySelector('.SearchResults')
 
-SearchBtn.addEventListener('click', () => {
-  SearchResults.classList.add('SearchResultsOn')
-  SearchResults.innerHTML = ''
-  let user = SearchText.value
- 
-  fetch('./Src/Data.json')
-    .then(res => res.json())
-    .then(data => {
-      //console.log(data)
-      let result =  data.filter(e => e.keyWord.includes(user))
-      console.log(result)
-    })
-    .catch(error => console.log(error)) 
-})*/
 
-// set & get All sound types 
-let SoundsByType =
-  document.querySelector('.SoundsByType')
+// Home page 
+// Say Hi To User 
+let UserBox =
+  document.querySelector('.UserBox')
+  
+let Name = getUserData.userName
+let date = new Date()
+let H = date.getHours()
+let M = date.getMinutes()
+let Time = H + ':' + M
+
+if (Time >= '06:00' && Time < '12:00') {
+  let user = Name === "" ? "Guest" : Name
+  let message = "Good Mornin 💫"
+  UserBox.innerHTML = 
+  `<p class="Message">${message}<br> Ms. <strong class="User"> ${user}</strong></p>`
+}
+else if (Time >= '12:00' && Time < '18:00') {
+  let user = Name === "" ? "Guest" : Name
+  let message = "Good Afternoon 💫"
+  UserBox.innerHTML =
+    `<p class="Message">${message}<br> Ms. <strong class="User"> ${user}</strong></p>`
+}
+else if (Time >= '18:00' && Time < '22:00') {
+  let user = Name === "" ? "Guest" : Name
+  let message = "Good Evening 🙃"
+  UserBox.innerHTML =
+    `<p class="Message">${message}<br> Ms. <strong class="User"> ${user}</strong></p>`
+}
+else {
+  let user = Name === "" ? "Guest" : Name
+  let message = "Good Night 🌛"
+  UserBox.innerHTML =
+    `<p class="Message">${message}<br> Ms. <strong class="User"> ${user}</strong></p>`
+}
+  
+
+// set & get All Alboms types 
+let AlbomsByType =
+  document.querySelector('.AlbomsByType')
+
 let AllType = 
 [
  {
   id: 1,
   name: "Water",
   cover: "https://res.cloudinary.com/dpgrn48l4/image/upload/v1711129761/Sleep%20App/Data/Water/Cover/Cover1.jpg",
-  sound: "4 Sound"
+  sound: "12 Sound"
  },
  {
    id: 2,
-   name: "Thunder",
+   name: "Storms",
    cover: "https://res.cloudinary.com/dpgrn48l4/image/upload/v1711130080/Sleep%20App/Data/Thunder/Cover/Cover1.jpg",
-   sound: "4 Sound"
+   sound: "14 Sound"
  },
  {
    id: 3,
@@ -155,25 +361,26 @@ let AllType =
 ]
 
 AllType.map(e => {
-  let Sound =
+  let Albom =
     document.createElement('div')
-  Sound.className = "SoundBox"
-  Sound.id = e.name 
-  Sound.cover = e.cover
-  Sound.setAttribute("onclick", "openAlbum(this)")
-  Sound.innerHTML = 
+  Albom.className = "AlbomBox"
+  Albom.id = e.name 
+  Albom.cover = e.cover
+  Albom.setAttribute("onclick", "openAlbum(this)")
+  Albom.innerHTML = 
   `<img src=${e.cover}>
-    <div class="SoundInfo">
+    <div class="AlbomInfo">
       <p>${e.name}</p>
       <small>${e.sound}</small>
     </div>`
-  SoundsByType.append(Sound)
+  AlbomsByType.append(Albom)
 })
 
-// get Album Data 
 
+// get Album Data 
 function openAlbum(e) {
   let cover = e.cover
+  let num = 0;
   let user = `${e.id}`
   fetch('./Src/Data.json')
     .then(res => res.json())
@@ -181,9 +388,6 @@ function openAlbum(e) {
       let results = data.filter(e => e.type === user)
       let MusicList =
         document.querySelector('.MusicList')
-      setTimeout(() => {
-        MusicList.style.right = "0"
-      }, 60)
       MusicList.style.display = 'flex'
       let MusicListBar =
         document.createElement('div')
@@ -196,7 +400,7 @@ function openAlbum(e) {
       MusicListBar.append(Back)
       let Title =
         document.createElement('h1')
-      Title.textContent = "Music List"
+      Title.textContent = "Sounds List"
       MusicListBar.append(Title)
       let MusicDiv =
         document.createElement('div')
@@ -206,9 +410,9 @@ function openAlbum(e) {
         let Music =
           document.createElement('div')
         Music.className = "Music"
-        Music.id = e.name
         Music.cover = cover
         Music.url = e.url
+        Music.user = user
         Music.setAttribute('onclick', 'openMusicPalyer(this)')
         MusicDiv.append(Music)
         let MusicImg =
@@ -217,7 +421,7 @@ function openAlbum(e) {
         Music.append(MusicImg)
         let MusicName =
           document.createElement('p')
-        MusicName.textContent = e.name
+        MusicName.innerHTML = `${user} sound ${num = num + 1}<br><small>${user} Album</small>`
         Music.append(MusicName)
         let Play =
           document.createElement('img')
@@ -226,30 +430,24 @@ function openAlbum(e) {
         let Musichr =
           document.createElement('hr')
         Musichr.className = "Musichr"
-        MusicDiv.append(Musichr)
+        //MusicDiv.append(Musichr)
       })
     })
     .catch(error => console.log(error))
 }
-
 function closeMusicList() {
   let MusicList =
     document.querySelector('.MusicList')
-  MusicList.style.right = "-100%"
-  setTimeout(() => {
-    MusicList.style.display = 'none'
-    MusicList.innerHTML = ''
-  }, 200)
+  MusicList.innerHTML = ''
+  MusicList.style.display = 'none'
 }
-
 function openMusicPalyer(e) {
-
+  let num = 0;
   let MusicPlayer =
     document.querySelector('.MusicPlayer')
-  
   setTimeout(() => {
     MusicPlayer.style.right = '0' 
-  }, 60)
+  }, 40)
   MusicPlayer.style.display = 'flex'
   let MusicPlayeBar = 
   document.createElement('div')
@@ -262,7 +460,7 @@ function openMusicPalyer(e) {
   MusicPlayeBar.append(Back)
   let Title =
     document.createElement('h1')
-  Title.textContent = "Music Player"
+  Title.textContent = "Sound Player"
   MusicPlayeBar.append(Title)
   let MPlayerItems =
     document.createElement('div')
@@ -278,7 +476,7 @@ function openMusicPalyer(e) {
     document.createElement('div')
   MusicName.className = 'MusicName'
   MusicName.innerHTML =
-    `<p>${e.id}</p>`
+    `<p>${e.user} Sound</p>`
   MPlayerItems.append(MusicName)
   let TimeLineBox =
     document.createElement('div')
@@ -319,8 +517,8 @@ function openMusicPalyer(e) {
   let TheAudio =
     document.querySelector('.audio')
   let PlayBtn =
-    document.querySelector('.container input')
-    
+   document.querySelector('.container input')
+  
   PlayBtn.addEventListener('click', () => {
     if (PlayBtn.checked) {
       TheAudio.play()
@@ -328,30 +526,38 @@ function openMusicPalyer(e) {
       TheAudio.pause()
     }
   })
-  
+   
   let BackBtn =
     document.querySelector('.BackBtn')
-  BackBtn.addEventListener('click', () => {
-    TheAudio.currentTime -= 10;
-  })
   let MoveBtn =
     document.querySelector('.MoveBtn')
+    
+  BackBtn.addEventListener('click', () => {
+    TheAudio.currentTime -= 10;
+    BackBtn.style.transform = 'scale(0.7, 1.5)'
+    setTimeout(() => {
+      BackBtn.style.transform = ''
+    }, 200)
+  })
   MoveBtn.addEventListener('click', () => {
     TheAudio.currentTime += 10;
+    MoveBtn.style.transform = 'scale(0.9, 1.5)'
+    setTimeout(() => {
+      MoveBtn.style.transform = ''
+    }, 200)
   })
- 
-  let LineTime = 
-    document.querySelector('.TimeLine')
   let StartTime =
     document.querySelector('.StartTime')
-  let EndTime = 
+  let EndTime =
     document.querySelector('.EndTime')
+  let LineTime =
+    document.querySelector('.TimeLine')
   let Progress =
     document.createElement('div')
   Progress.className = 'progress'
   LineTime.appendChild(Progress)
   
-  TheAudio.addEventListener('timeupdate',function() {
+  TheAudio.addEventListener('timeupdate', () => {
     let duration = TheAudio.duration;
     let currentTime = TheAudio.currentTime;
     let progressWidth = (currentTime / duration) * 100;
@@ -366,8 +572,8 @@ function openMusicPalyer(e) {
     remainingSeconds = (remainingSeconds < 10) ? "0" + remainingSeconds : remainingSeconds;
     return minutes + ":" + remainingSeconds;
   }
+  
 }
-
 function closeMusicPlayer() {
   let MusicPlayer =
     document.querySelector('.MusicPlayer')
@@ -377,4 +583,3 @@ function closeMusicPlayer() {
     MusicPlayer.innerHTML = ''
   }, 200)
 }
-
